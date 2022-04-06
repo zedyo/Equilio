07419994a0473d583843de4c82a7a29b7bf01236
Nikolai Seel <46816354+zedyo@users.noreply.github.com>
2022-04-06 16:49:15 +0200

    Fix DateSelector

--- public/js/app.js
+++ public/js/app.js
@@ -8348,17 +8348,6 @@ function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { va
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 
 
@@ -8369,16 +8358,7 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 
 function DateSelector(props) {
-  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)({
-    month: "".concat(moment__WEBPACK_IMPORTED_MODULE_1___default()().format('M')),
-    year: "".concat(moment__WEBPACK_IMPORTED_MODULE_1___default()().format('YYYY'))
-  }),
-      _useState2 = _slicedToArray(_useState, 2),
-      dateSlectorInput = _useState2[0],
-      setDateSelectorInput = _useState2[1];
-
-  console.log(moment__WEBPACK_IMPORTED_MODULE_1___default()("".concat(props.dateSelectorData.month), 'M').format('MMM'));
-  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.Fragment, {
     children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(react_bootstrap__WEBPACK_IMPORTED_MODULE_4__["default"], {
       children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(react_bootstrap__WEBPACK_IMPORTED_MODULE_5__["default"], {
         children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(react_bootstrap__WEBPACK_IMPORTED_MODULE_6__["default"], {
@@ -8393,7 +8373,7 @@ function DateSelector(props) {
                 props.dateSelectorData.month === '1' ? props.setDateSelector({
                   month: '12',
                   year: (Number(props.dateSelectorData.year) - 1).toString()
-                }) : props.setDateSelector(_objectSpread(_objectSpread({}, dateSlectorInput), {}, {
+                }) : props.setDateSelector(_objectSpread(_objectSpread({}, props.dateSelectorData), {}, {
                   month: (Number(props.dateSelectorData.month) - 1).toString()
                 }));
               },
@@ -8411,7 +8391,7 @@ function DateSelector(props) {
                 props.dateSelectorData.month === '12' ? props.setDateSelector({
                   month: '1',
                   year: (Number(props.dateSelectorData.year) + 1).toString()
-                }) : props.setDateSelector(_objectSpread(_objectSpread({}, dateSlectorInput), {}, {
+                }) : props.setDateSelector(_objectSpread(_objectSpread({}, props.dateSelectorData), {}, {
                   month: (Number(props.dateSelectorData.month) + 1).toString()
                 }));
               },
