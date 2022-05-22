27ab8a6acf1dcae2d6353e7c5718b34016b3096a
Nikolai Seel <46816354+zedyo@users.noreply.github.com>
2022-05-22 16:56:33 +0200

    Preference implementation

--- public/js/app.js
+++ public/js/app.js
@@ -11278,8 +11278,7 @@ function DutyCell(props) {
   var dispatch = (0,react_redux__WEBPACK_IMPORTED_MODULE_1__.useDispatch)();
   var wish = props.employeeWish.find(function (d) {
     return d.day == props.day && d.month == props.month;
-  }); //TODO: CSS Hervorhebung, falls Wish und Duty nicht übereinstimmen
-  //TODO: Etwas helleres grau eines Wunsches
+  }); //TODO: Etwas helleres grau eines Wunsches
   //TODO: Error Handling bei Inputeingabe
 
   if (employeeDuty) {
@@ -11313,34 +11312,10 @@ function DutyCell(props) {
     var color = DutyColor;
     var inputColor = {
       color: color
-    }; // async function sendDuty(value, day, month, year, employee_id) {
-    //   if (value !== '') {
-    //     try {
-    //       const { data } = await axios.patch(
-    //         `http://127.0.0.1:8000/api/duty/`,
-    //         {
-    //           value,
-    //           day,
-    //           month,
-    //           year,
-    //           employee_id,
-    //         }
-    //       )
-    //       const hex = data.new_duty.shift.color_hex
-    //       if (data.length !== 0 || hex) {
-    //         setDutyColor(data.new_duty.shift.color_hex)
-    //         setCellStyle('inputDutyForm')
-    //       }
-    //     } catch (error) {
-    //       setCellStyle('error')
-    //       setInputDuty('')
-    //     }
-    //   }
-    // }
-
+    };
     return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("input", {
       style: inputColor,
-      className: duty === undefined ? CellStyle : duty.wish_injury == true ? 'wishInjury' : duty.preference_injury == true ? 'preferenceInjury' : wish !== undefined ? 'wishForm' : CellStyle,
+      className: duty === undefined ? CellStyle : duty.wish_injury == true ? 'wishInjury' : duty.preference_injury == true && wish == undefined ? 'preferenceInjury' : wish !== undefined ? 'wishForm' : CellStyle,
       value: inputDutyValue,
       placeholder: wish !== undefined ? wish.shift.abrv : '',
       onChange: function onChange(e) {
@@ -11359,16 +11334,7 @@ function DutyCell(props) {
           year: props.year,
           employee_id: props.employeeId
         }));
-      } // onBlur={(e) =>
-      //   sendDuty(
-      //     inputDutyValue,
-      //     props.day,
-      //     props.month,
-      //     props.year,
-      //     props.employeeId
-      //   )
-      // }
-
+      }
     });
   }
 
@@ -12357,18 +12323,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
 /* harmony import */ var _features_preferences_preferenceSlice__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../../features/preferences/preferenceSlice */ "./resources/js/features/preferences/preferenceSlice.js");
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
 
 
 
@@ -12380,11 +12334,6 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 function Preferences(props) {
   var dispatch = (0,react_redux__WEBPACK_IMPORTED_MODULE_1__.useDispatch)();
 
-  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)({}),
-      _useState2 = _slicedToArray(_useState, 2),
-      preferenceData = _useState2[0],
-      setPreference = _useState2[1];
-
   var _useSelector = (0,react_redux__WEBPACK_IMPORTED_MODULE_1__.useSelector)(function (store) {
     return store.shifts;
   }),
@@ -12394,6 +12343,11 @@ function Preferences(props) {
     return store.shiftTypes;
   }),
       shiftTypesData = _useSelector2.shiftTypesData;
+
+  var _useSelector3 = (0,react_redux__WEBPACK_IMPORTED_MODULE_1__.useSelector)(function (store) {
+    return store.preferences;
+  }),
+      preferenceData = _useSelector3.preferenceData;
 
   return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.Fragment, {
     children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(react_bootstrap__WEBPACK_IMPORTED_MODULE_4__["default"], {
@@ -12416,6 +12370,10 @@ function Preferences(props) {
                     return shift.shift_type.id === shiftType.id;
                   }).map(function (shift) {
                     return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(react_bootstrap__WEBPACK_IMPORTED_MODULE_8__["default"].Check, {
+                      checked: //TODO: Fehler beim Ändern der Präferenz
+                      preferenceData.find(function (preference) {
+                        return preference.employee_id == props.employeeId && preference.shift_id == shift.id;
+                      }) ? true : false,
                       type: "switch",
                       id: shift.id,
                       label: shift.abrv,
